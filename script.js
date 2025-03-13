ae602bb9069604ed28915f63df2cef03342d8555
dancampbelldev <dancampbelluw@gmail.com>
2025-03-13 14:53:53 +0000

    Update script.js
    Popup image background made none-scrollable

--- script.js
+++ script.js
@@ -65,9 +65,11 @@ document.querySelectorAll(".photos-container img").forEach(image => {
   image.onclick = () => {
     document.querySelector(".popup-image").style.display = 'block';
     document.querySelector(".popup-image img").src = image.getAttribute('src');
+    document.querySelector("body").style.overflow = 'hidden';
   }
 });
 
 document.querySelector(".popup-image span").onclick = () => {
   document.querySelector(".popup-image").style.display = 'none';
+  document.querySelector("body").style.overflow = 'auto';
 }
